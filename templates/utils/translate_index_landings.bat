0ef500c5bb6c0cc2e6b05b36ee5304140c997f5f
Albert Akhmetov <akhmetov@live.com>
2024-02-13 22:02:01 +0500

    Update Parser ru-content

--- templates/utils/translate_index_landings.bat
+++ templates/utils/translate_index_landings.bat
@@ -1,32 +1,32 @@
-.\Translation\Bin\net6.0\Res.Translator.exe -r ..\data\parse\en.json -d de --overwrite
-ren "..\data\parse\en.de.json" "de.json"
+.\Translation\Bin\net6.0\Res.Translator.exe -r ..\data\_index\en.json -d de --overwrite
+ren "..\data\_index\en.de.json" "de.json"
 
-.\Translation\Bin\net6.0\Res.Translator.exe -r ..\data\parse\en.json -d es --overwrite
-ren "..\data\parse\en.es.json" "es.json"
+.\Translation\Bin\net6.0\Res.Translator.exe -r ..\data\_index\en.json -d es --overwrite
+ren "..\data\_index\en.es.json" "es.json"
 
-.\Translation\Bin\net6.0\Res.Translator.exe -r ..\data\parse\en.json -d fr --overwrite
-ren "..\data\parse\en.fr.json" "fr.json"
+.\Translation\Bin\net6.0\Res.Translator.exe -r ..\data\_index\en.json -d fr --overwrite
+ren "..\data\_index\en.fr.json" "fr.json"
 
-.\Translation\Bin\net6.0\Res.Translator.exe -r ..\data\parse\en.json -d id --overwrite
-ren "..\data\parse\en.id.json" "id.json"
+.\Translation\Bin\net6.0\Res.Translator.exe -r ..\data\_index\en.json -d id --overwrite
+ren "..\data\_index\en.id.json" "id.json"
 
-.\Translation\Bin\net6.0\Res.Translator.exe -r ..\data\parse\en.json -d it --overwrite
-ren "..\data\parse\en.it.json" "it.json"
+.\Translation\Bin\net6.0\Res.Translator.exe -r ..\data\_index\en.json -d it --overwrite
+ren "..\data\_index\en.it.json" "it.json"
 
-.\Translation\Bin\net6.0\Res.Translator.exe -r ..\data\parse\en.json -d ja --overwrite
-ren "..\data\parse\en.ja.json" "ja.json"
+.\Translation\Bin\net6.0\Res.Translator.exe -r ..\data\_index\en.json -d ja --overwrite
+ren "..\data\_index\en.ja.json" "ja.json"
 
-.\Translation\Bin\net6.0\Res.Translator.exe -r ..\data\parse\en.json -d ko --overwrite
-ren "..\data\parse\en.ko.json" "ko.json"
+.\Translation\Bin\net6.0\Res.Translator.exe -r ..\data\_index\en.json -d ko --overwrite
+ren "..\data\_index\en.ko.json" "ko.json"
 
-.\Translation\Bin\net6.0\Res.Translator.exe -r ..\data\parse\en.json -d pt --overwrite
-ren "..\data\parse\en.pt.json" "pt.json"
+.\Translation\Bin\net6.0\Res.Translator.exe -r ..\data\_index\en.json -d pt --overwrite
+ren "..\data\_index\en.pt.json" "pt.json"
 
-.\Translation\Bin\net6.0\Res.Translator.exe -r ..\data\parse\en.json -d pl --overwrite
-ren "..\data\parse\en.pl.json" "pl.json"
+.\Translation\Bin\net6.0\Res.Translator.exe -r ..\data\_index\en.json -d pl --overwrite
+ren "..\data\_index\en.pl.json" "pl.json"
 
-.\Translation\Bin\net6.0\Res.Translator.exe -r ..\data\parse\en.json -d ru --overwrite
-ren "..\data\parse\en.ru.json" "ru.json"
+.\Translation\Bin\net6.0\Res.Translator.exe -r ..\data\_index\en.json -d ru --overwrite
+ren "..\data\_index\en.ru.json" "ru.json"
 
-.\Translation\Bin\net6.0\Res.Translator.exe -r ..\data\parse\en.json -d zh-CN --overwrite
-ren "..\data\parse\en.zh-CN.json" "zh.json"
+.\Translation\Bin\net6.0\Res.Translator.exe -r ..\data\_index\en.json -d zh-CN --overwrite
+ren "..\data\_index\en.zh-CN.json" "zh.json"
